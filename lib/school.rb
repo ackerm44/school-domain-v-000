55010cf7f0de4d812f69841ff321a9320912db94
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 03:04:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_reader :roster
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster[grade].each do |grade|
+      grade.sort
+    end
+  end
+end
